3b9b8287d063fe3de1046043f4f00a1eaa897567
johnche(车雄生) <johnche@tencent.com>
2017-03-01 16:25:23 +0800

    linux库及编译脚本

---
+++ build/make_linux32_lua53.sh
@@ -0,0 +1,5 @@
+mkdir -p build_linux32 && cd build_linux32
+cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 ../
+cd ..
+cmake --build build_linux32 --config Release
+
