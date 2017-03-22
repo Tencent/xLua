882fde4df654fcd65cfe874a1bb7f39f8bc5d4bb
johnche(车雄生) <johnche@tencent.com>
2017-03-22 09:54:19 +0800

    uwp平台的64位及arm库

--- build/make_uwp.bat
+++ build/make_uwp.bat
@@ -2,4 +2,14 @@ mkdir build_uwp & pushd build_uwp
 cmake -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
 popd
 cmake --build build_uwp --config Release
+
+mkdir build_uwp64 & pushd build_uwp64
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
+popd
+cmake --build build_uwp64 --config Release
+
+mkdir build_uwp_arm & pushd build_uwp_arm
+cmake -G "Visual Studio 14 2015 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
+popd
+cmake --build build_uwp_arm --config Release
 pause
