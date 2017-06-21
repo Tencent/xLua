1c454db9eae22848324046368e615d82892b60cd
johnche <chexiongsheng@qq.com>
2017-06-21 16:22:42 +0800

    pinvoke hotfix flags in iOS

--- build/xlua.c
+++ build/xlua.c
@@ -1202,16 +1202,22 @@ LUA_API int xlua_get_hotfix_flag(int idx) {
 }
 
 LUA_API void xlua_set_hotfix_flag(int idx, int flag) {
-	int i;
+	int i = 0;
+        int* new_hotfix_flags = NULL;
 	if (idx >= xlua_hotfix_flags_len) {
 		if (xlua_hotfix_flags == NULL) {
 			xlua_hotfix_flags = (int*)malloc((idx + 1) * sizeof(int));
 		} else {
-			xlua_hotfix_flags = (int*)realloc(xlua_hotfix_flags, (idx + 1) * sizeof(int));
+			new_hotfix_flags = (int*)realloc(xlua_hotfix_flags, (idx + 1) * sizeof(int));
+                        if (NULL == new_hotfix_flags) { // just skip operation
+                            return;
+                        }
+                        xlua_hotfix_flags = new_hotfix_flags;
 		}
 		for(i = xlua_hotfix_flags_len; i < (idx + 1); i++) {
 			xlua_hotfix_flags[i] = 0;
 		}
+                xlua_hotfix_flags_len = idx + 1;
 	} 
 	xlua_hotfix_flags[idx] = flag;
 }
