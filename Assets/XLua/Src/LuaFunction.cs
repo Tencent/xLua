bd37c7d9a3f6349e352795c75e3c2feaf1c6808d
johnche(车雄生) <johnche@tencent.com>
2017-06-23 09:39:09 +0800

    LuaFunction.SetEnv在lua53下工作不符合预期，先在lua53禁用

--- Assets/XLua/Src/LuaFunction.cs
+++ Assets/XLua/Src/LuaFunction.cs
@@ -210,6 +210,10 @@ namespace XLua
             lock (luaEnv.luaEnvLock)
             {
 #endif
+                if (LuaIndexes.LUA_REGISTRYINDEX != -10000)
+                {
+                    throw new InvalidOperationException("SetEnv not support in lua53");
+                }
                 var L = luaEnv.L;
                 int oldTop = LuaAPI.lua_gettop(L);
                 push(L);
