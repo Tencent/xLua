9eddcabbb6d77a89be0701f83b804968efed37bb
johnche <chexiongsheng@qq.com>
2017-08-01 14:09:27 +0800

    Add CI for luajit version

---
+++ build/make_linux32_luajit.sh
@@ -0,0 +1,9 @@
+cd luajit-2.1.0b2
+make clean
+make CC="gcc -m32"
+cd ..
+mkdir -p build_linux32_lj && cd build_linux32_lj
+cmake -DUSING_LUAJIT=ON -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 ../
+cd ..
+cmake --build build_linux32_lj --config Release
+
