1359a2eedd50815d852598e98cfbffcd1fa23e79
johnche(车雄生) <johnche@tencent.com>
2017-10-27 15:39:26 +0800

    尝试通过更改mono运行时去掉warning后，mac下不可用

--- Assets/XLua/Src/Editor/Hotfix.cs
+++ Assets/XLua/Src/Editor/Hotfix.cs
@@ -1058,18 +1058,10 @@ namespace XLua
 
 #if UNITY_EDITOR_OSX
 			var mono_path = Path.Combine(Path.GetDirectoryName(typeof(UnityEngine.Debug).Module.FullyQualifiedName),
-#if UNITY_5_5_OR_NEWER
-            "../Mono/bin/mono");
-#else
-            "../MonoBleedingEdge/bin/mono");
-#endif
+				"../MonoBleedingEdge/bin/mono");
 #elif UNITY_EDITOR_WIN
             var mono_path = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName),
-#if UNITY_5_5_OR_NEWER
-                "Data/Mono/bin/mono.exe");
-#else
                 "Data/MonoBleedingEdge/bin/mono.exe");
-#endif
 #endif
             var inject_tool_path = "./Tools/XLuaHotfixInject.exe";
             if (!File.Exists(inject_tool_path))
