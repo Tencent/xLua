6f9e176ef08cf498df63da6b27a9deba8bd10b06
huangjunjie <hwangjunjie@outlook.com>
2018-06-21 15:27:49 +0800

    实现interface的事件跟索引映射

--- Assets/XLua/Examples/04_LuaObjectOrented/InvokeLua.cs
+++ Assets/XLua/Examples/04_LuaObjectOrented/InvokeLua.cs
@@ -6,17 +6,31 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
+using System;
 using UnityEngine;
-using System.Collections;
 using XLua;
+
+[CSharpCallLua]
+public delegate void EventHandler<TEvent>(object sender, TEvent e) where TEvent : EventArgs;
+
+[CSharpCallLua]
+public class PropertyChangedEventArgs : EventArgs
+{
+    public string name;
+    public object value;
+}
 
 public class InvokeLua : MonoBehaviour
 {
     [CSharpCallLua]
     public interface ICalc
     {
+        event EventHandler<PropertyChangedEventArgs> PropertyChanged;
+
         int Add(int a, int b);
         int Mult { get; set; }
+
+        object this[int index] { get; set; }
     }
 
     [CSharpCallLua]
@@ -27,14 +41,33 @@ public class InvokeLua : MonoBehaviour
                     __index = {
                         Add = function(self, a, b)
                             return (a + b) * self.Mult
-                        end
+                        end,
+                        
+                        get_Item = function(self, index)
+                            return self.list[index + 1]
+                        end,
+
+                        set_Item = function(self, index, value)
+                            self.list[index + 1] = value
+                            if self.notify ~= nil then
+                                self.notify(self, {name = index, value = value})
+                            end
+                        end,
+                        
+                        add_PropertyChanged = function(self, delegate)
+                            self.notify = delegate
+                        end,
+                                                
+                        remove_PropertyChanged = function(self, delegate)
+                            self.notify = nil
+                        end,
                     }
                 }
 
                 Calc = {
 	                New = function (mult, ...)
                         print(...)
-                        return setmetatable({Mult = mult}, calc_mt)
+                        return setmetatable({Mult = mult, list = {'a','b','c'}}, calc_mt)
                     end
                 }
 	        ";
@@ -54,6 +87,24 @@ public class InvokeLua : MonoBehaviour
         Debug.Log("sum(*10) =" + calc.Add(1, 2));
         calc.Mult = 100;
         Debug.Log("sum(*100)=" + calc.Add(1, 2));
+
+        Debug.Log("list[0]=" + calc[0]);
+        Debug.Log("list[1]=" + calc[1]);
+        
+        calc.PropertyChanged += Notify;
+
+        calc[1] = "d";
+        Debug.Log("list[1]=" + calc[1]);
+
+        calc.PropertyChanged -= Notify;
+
+        calc[1] = "e";
+        Debug.Log("list[1]=" + calc[1]);
+    }
+
+    void Notify(object sender, PropertyChangedEventArgs e)
+    {
+        Debug.Log(string.Format("{0} has property changed {1}={2}", sender, e.name, e.value));
     }
 
     // Update is called once per frame
