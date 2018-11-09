5a945412ffd48acf45bd9a50b74e77f5492c5ec4
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2018-11-09 13:54:22 +0800

    以RawObject方式传递byte[]的例子

--- Assets/XLua/Examples/11_RawObject/RawObjectTest.cs
+++ Assets/XLua/Examples/11_RawObject/RawObjectTest.cs
@@ -1,13 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UnityEngine;
 using XLua;
 
 namespace XLuaTest
 {
+    public class RawByteArray : RawObject
+    {
+        public byte[] m_Target;
+        public object Target { get { return m_Target; } }
+
+        public RawByteArray(int length)
+        {
+            m_Target = new byte[length];
+        }
+        public RawByteArray(byte[] data)
+        {
+            m_Target = data;
+        }
+
+        public byte this[int index]
+        {
+            get { return m_Target[index]; }
+            set { m_Target[index] = value; }
+        }
+    }
+
     public class RawObjectTest : MonoBehaviour
     {
         public static void PrintType(object o)
         {
             Debug.Log("type:" + o.GetType() + ", value:" + o);
+        }
+
+        public static byte[] BytesFromString(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
         }
 
         // Use this for initialization
@@ -18,6 +45,10 @@ namespace XLuaTest
             luaenv.DoString("CS.XLuaTest.RawObjectTest.PrintType(1234)");
             //通过一个继承RawObject的类，能实现指明以一个int来传递
             luaenv.DoString("CS.XLuaTest.RawObjectTest.PrintType(CS.XLua.Cast.Int32(1234))");
+            //直接传递byte[]，lua会作为string处理
+            luaenv.DoString("CS.XLuaTest.RawObjectTest.PrintType(CS.XLuaTest.RawObjectTest.BytesFromString('string from lua'))");
+            //通过继承RawObject的RawByteArray，实现string以byte[]方式传递
+            luaenv.DoString("CS.XLuaTest.RawObjectTest.PrintType(CS.XLuaTest.RawByteArray('string from lua'))");
             luaenv.Dispose();
         }
 
