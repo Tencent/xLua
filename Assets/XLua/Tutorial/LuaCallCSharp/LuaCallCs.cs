279712d02caad111ca4c3f41c4a75060b1c48c06
johnche(车雄生) <johnche@tencent.com>
2019-01-28 16:55:02 +0800

    有个地方只适合做测试用例，而不是教程，删了

--- Assets/XLua/Tutorial/LuaCallCSharp/LuaCallCs.cs
+++ Assets/XLua/Tutorial/LuaCallCSharp/LuaCallCs.cs
@@ -45,40 +45,6 @@ namespace Tutorial
 		E2
 	}
 
-	[LuaCallCSharp]
-	public class PrivateOverrideClass
-	{
-
-		public void TestFunc(int i)
-		{
-			Debug.Log("TestFunc(int i), i = " + i);
-		}
-
-		public void TestFunc(string i)
-		{
-			Debug.Log("TestFunc(string i), i = " + i);
-		}
-
-		private void TestFunc(double i)
-		{
-			Debug.Log("TestFunc(double i), i = " + i);
-		}
-
-		public void TestFunc3(int i)
-		{
-			Debug.Log("TestFunc(int i), i = " + i);
-		}
-
-		public void TestFunc2(string i)
-		{
-			Debug.Log("TestFunc(string i), i = " + i);
-		}
-
-		private void TestFunc2(double i)
-		{
-			Debug.Log("TestFunc(double i), i = " + i);
-		}
-	}
 
 	[LuaCallCSharp]
 	public class DerivedClass : BaseClass
@@ -251,78 +217,69 @@ public class LuaCallCs : MonoBehaviour
             print('ComplexFunc ret:', ret, p2, p3, csfunc)
             csfunc()
 
-           --重载方法调用
-           testobj:TestFunc(100)
-           testobj:TestFunc('hello')
-		   
-		   --xlua.private_accessible访问私有方法
-		   local PrivateOverrideClass = CS.Tutorial.PrivateOverrideClass
-		   local priObj=PrivateOverrideClass()
-		   priObj:TestFunc(100.0)
-	       priObj:TestFunc('hello')
-		   xlua.private_accessible(PrivateOverrideClass);
-		   priObj:TestFunc(100.0)
-		   priObj:TestFunc('hello')
+            --重载方法调用
+            testobj:TestFunc(100)
+            testobj:TestFunc('hello')
 
-           --操作符
-           local testobj2 = DerivedClass()
-           testobj2.DMF = 2048
-           print('(testobj + testobj2).DMF = ', (testobj + testobj2).DMF)
+            --操作符
+            local testobj2 = DerivedClass()
+            testobj2.DMF = 2048
+            print('(testobj + testobj2).DMF = ', (testobj + testobj2).DMF)
 
-           --默认值
-           testobj:DefaultValueFunc(1)
-           testobj:DefaultValueFunc(3, 'hello', 'john')
+            --默认值
+            testobj:DefaultValueFunc(1)
+            testobj:DefaultValueFunc(3, 'hello', 'john')
 
-           --可变参数
-           testobj:VariableParamsFunc(5, 'hello', 'john')
+            --可变参数
+            testobj:VariableParamsFunc(5, 'hello', 'john')
 
-           --Extension methods
-           print(testobj:GetSomeData()) 
-           print(testobj:GetSomeBaseData()) --访问基类的Extension methods
-           testobj:GenericMethodOfString()  --通过Extension methods实现访问泛化方法
+            --Extension methods
+            print(testobj:GetSomeData()) 
+            print(testobj:GetSomeBaseData()) --访问基类的Extension methods
+            testobj:GenericMethodOfString()  --通过Extension methods实现访问泛化方法
 
-           --枚举类型
-           local e = testobj:EnumTestFunc(CS.Tutorial.TestEnum.E1)
-           print(e, e == CS.Tutorial.TestEnum.E2)
-           print(CS.Tutorial.TestEnum.__CastFrom(1), CS.Tutorial.TestEnum.__CastFrom('E1'))
-           print(CS.Tutorial.DerivedClass.TestEnumInner.E3)
-           assert(CS.Tutorial.BaseClass.TestEnumInner == nil)
+            --枚举类型
+            local e = testobj:EnumTestFunc(CS.Tutorial.TestEnum.E1)
+            print(e, e == CS.Tutorial.TestEnum.E2)
+            print(CS.Tutorial.TestEnum.__CastFrom(1), CS.Tutorial.TestEnum.__CastFrom('E1'))
+            print(CS.Tutorial.DerivedClass.TestEnumInner.E3)
+            assert(CS.Tutorial.BaseClass.TestEnumInner == nil)
 
-           --Delegate
-           testobj.TestDelegate('hello') --直接调用
-           local function lua_delegate(str)
-               print('TestDelegate in lua:', str)
-           end
-           testobj.TestDelegate = lua_delegate + testobj.TestDelegate --combine，这里演示的是C#delegate作为右值，左值也支持
-           testobj.TestDelegate('hello')
-           testobj.TestDelegate = testobj.TestDelegate - lua_delegate --remove
-           testobj.TestDelegate('hello')
+            --Delegate
+            testobj.TestDelegate('hello') --直接调用
+            local function lua_delegate(str)
+                print('TestDelegate in lua:', str)
+            end
+            testobj.TestDelegate = lua_delegate + testobj.TestDelegate --combine，这里演示的是C#delegate作为右值，左值也支持
+            testobj.TestDelegate('hello')
+            testobj.TestDelegate = testobj.TestDelegate - lua_delegate --remove
+            testobj.TestDelegate('hello')
 
-           --事件
-           local function lua_event_callback1() print('lua_event_callback1') end
-           local function lua_event_callback2() print('lua_event_callback2') end
-           testobj:TestEvent('+', lua_event_callback1)
-           testobj:CallEvent()
-           testobj:TestEvent('+', lua_event_callback2)
-           testobj:CallEvent()
-           testobj:TestEvent('-', lua_event_callback1)
-           testobj:CallEvent()
-           testobj:TestEvent('-', lua_event_callback2)
+            --事件
+            local function lua_event_callback1() print('lua_event_callback1') end
+            local function lua_event_callback2() print('lua_event_callback2') end
+            testobj:TestEvent('+', lua_event_callback1)
+            testobj:CallEvent()
+            testobj:TestEvent('+', lua_event_callback2)
+            testobj:CallEvent()
+            testobj:TestEvent('-', lua_event_callback1)
+            testobj:CallEvent()
+            testobj:TestEvent('-', lua_event_callback2)
 
-           --64位支持
-           local l = testobj:TestLong(11)
-           print(type(l), l, l + 100, 10000 + l)
+            --64位支持
+            local l = testobj:TestLong(11)
+            print(type(l), l, l + 100, 10000 + l)
 
-           --typeof
-           newGameObj:AddComponent(typeof(CS.UnityEngine.ParticleSystem))
+            --typeof
+            newGameObj:AddComponent(typeof(CS.UnityEngine.ParticleSystem))
 
-           --cast
-           local calc = testobj:GetCalc()
-           print('assess instance of InnerCalc via reflection', calc:add(1, 2))
-           assert(calc.id == 100)
-           cast(calc, typeof(CS.Tutorial.ICalc))
-           print('cast to interface ICalc', calc:add(1, 2))
-           assert(calc.id == nil)
+            --cast
+            local calc = testobj:GetCalc()
+            print('assess instance of InnerCalc via reflection', calc:add(1, 2))
+            assert(calc.id == 100)
+            cast(calc, typeof(CS.Tutorial.ICalc))
+            print('cast to interface ICalc', calc:add(1, 2))
+            assert(calc.id == nil)
        end
 
        demo()
