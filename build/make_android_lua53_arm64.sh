92227f79c95f2287eabac58b68b5fdee553b8ed7
johnche <chexiongsheng@qq.com>
2019-02-27 15:38:11 +0800

    add android arm64 support for luajit version

--- build/make_android_lua53_arm64.sh
+++ build/make_android_lua53_arm64.sh
@@ -1,5 +1,5 @@
 if [ -z "$ANDROID_NDK" ]; then
-    export ANDROID_NDK=~/android-ndk-r16b
+    export ANDROID_NDK=~/android-ndk-r15c
 fi
 
 mkdir -p build_v7a && cd build_v7a
@@ -16,11 +16,11 @@ cmake --build build_v8a --config Release
 mkdir -p plugin_lua53/Plugins/Android/libs/arm64-v8a/
 cp build_v8a/libxlua.so plugin_lua53/Plugins/Android/libs/arm64-v8a/libxlua.so
 
-#mkdir -p build_x86 && cd build_x86
-#cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang3.5 -DANDROID_NATIVE_API_LEVEL=android-9 ../
-#cd ..
-#cmake --build build_x86 --config Release
-#mkdir -p plugin_lua53/Plugins/Android/libs/x86/
-#cp build_x86/libxlua.so plugin_lua53/Plugins/Android/libs/x86/libxlua.so
+mkdir -p build_x86 && cd build_x86
+cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang -DANDROID_NATIVE_API_LEVEL=android-9 ../
+cd ..
+cmake --build build_x86 --config Release
+mkdir -p plugin_lua53/Plugins/Android/libs/x86/
+cp build_x86/libxlua.so plugin_lua53/Plugins/Android/libs/x86/libxlua.so
 
 
