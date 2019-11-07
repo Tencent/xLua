a827966c5ba7c553836c7e7ea72eaad144f18405
johnche <johnche@tencent.com>
2019-11-07 18:45:06 +0800

    改为vs2017

--- build/make_win64_luajit_gc64.bat
+++ build/make_win64_luajit_gc64.bat
@@ -7,7 +7,7 @@ call msvcbuild_mt.bat gc64 static
 cd ..\..
 
 mkdir build_lj64 & pushd build_lj64
-cmake -DUSING_LUAJIT=ON -DGC64=ON -G "Visual Studio 14 2015 Win64" ..
+cmake -DUSING_LUAJIT=ON -DGC64=ON -G "Visual Studio 15 2017 Win64" ..
 IF %ERRORLEVEL% NEQ 0 cmake -DUSING_LUAJIT=ON -DGC64=ON -G "Visual Studio 15 2017 Win64" ..
 popd
 cmake --build build_lj64 --config Release
