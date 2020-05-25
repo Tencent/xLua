799a4fe88e60d2b1d11e10a2911c0c21898e4d9e
johnche <johnche@tencent.com>
2020-05-25 17:52:35 +0800

    unity 2019兼容，fix https://github.com/Tencent/xLua/issues/732

--- Assets/XLua/Src/Editor/Hotfix.cs
+++ Assets/XLua/Src/Editor/Hotfix.cs
@@ -24,6 +24,10 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using System.Diagnostics;
+#if UNITY_2019
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+#endif
 #endif
 
 namespace XLua
@@ -1585,6 +1589,18 @@ namespace XLua
 
 namespace XLua
 {
+#if UNITY_2019
+    class MyCustomBuildProcessor : IPostBuildPlayerScriptDLLs
+    {
+        public int callbackOrder { get { return 0; } }
+        public void OnPostBuildPlayerScriptDLLs(BuildReport report)
+        {
+            var dir = Path.GetDirectoryName(report.files.Single(file => file.path.EndsWith("Assembly-CSharp.dll")).path);
+            Hotfix.HotfixInject(dir);
+        }
+    }
+#endif
+
     public static class Hotfix
     {
         static bool ContainNotAsciiChar(string s)
@@ -1599,9 +1615,16 @@ namespace XLua
             return false;
         }
 
+#if !UNITY_2019
         [PostProcessScene]
+#endif
         [MenuItem("XLua/Hotfix Inject In Editor", false, 3)]
         public static void HotfixInject()
+        {
+            HotfixInject("./Library/ScriptAssemblies/");
+        }
+
+        public static void HotfixInject(string assemblyDir)
         {
             if (Application.isPlaying)
             {
