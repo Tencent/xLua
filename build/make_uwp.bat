2193680104fb4de9457b8880c6b87b65223c25c7
johnche <johnche@tencent.com>
2020-09-15 11:51:54 +0800

    添加uwp的arm64编译

--- build/make_uwp.bat
+++ build/make_uwp.bat
@@ -19,4 +19,11 @@ cmake --build build_uwp_arm --config Release
 md plugin_lua53\Plugins\WSA\ARM
 copy /Y build_uwp_arm\Release\xlua.dll plugin_lua53\Plugins\WSA\ARM\xlua.dll
 
+mkdir build_uwp_arm64 & pushd build_uwp_arm64
+cmake -G "Visual Studio 15 2017" -A ARM64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..
+popd
+cmake --build build_uwp_arm64 --config Release
+md plugin_lua53\Plugins\WSA\ARM64
+copy /Y build_uwp_arm64\Release\xlua.dll plugin_lua53\Plugins\WSA\ARM64\xlua.dll
+
 pause
