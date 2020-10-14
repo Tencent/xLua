ce5b25188067cbe4b3a43fe3b42a25aedea9d688
johnche <johnche@tencent.com>
2020-10-14 17:56:06 +0800

    添加lua53的自动编译

--- build/make_osx_lua54.sh
+++ build/make_osx_lua54.sh
@@ -1,7 +1,7 @@
-mkdir -p build_osx && cd build_osx
+mkdir -p build_osx_54 && cd build_osx_54
 cmake -DLUA_VERSION=5.4.1 -GXcode ../
 cd ..
-cmake --build build_osx --config Release
+cmake --build build_osx_54 --config Release
 mkdir -p plugin_lua54/Plugins/xlua.bundle/Contents/MacOS/
-cp build_osx/Release/xlua.bundle/Contents/MacOS/xlua plugin_lua54/Plugins/xlua.bundle/Contents/MacOS/xlua
+cp build_osx_54/Release/xlua.bundle/Contents/MacOS/xlua plugin_lua54/Plugins/xlua.bundle/Contents/MacOS/xlua
 
