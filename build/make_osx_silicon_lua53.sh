c9dd144e6d50b0a27575f8a9fc9e3f7dd34fb667
johnche <johnche@tencent.com>
2022-06-14 19:29:50 +0800

    编译报错

--- build/make_osx_silicon_lua53.sh
+++ build/make_osx_silicon_lua53.sh
@@ -1,7 +1,7 @@
 mkdir -p build_osx_silicon && cd build_osx_silicon
 cmake -GXcode ../
 cd ..
-cmake -DBUILD_SILICON --build build_osx_silicon --config Release
+cmake -DBUILD_SILICON=ON --build build_osx_silicon --config Release
 mkdir -p plugin_lua53/Plugins/arm64/
 cp build_osx_silicon/Release/libxlua.dylib plugin_lua53/Plugins/arm64/
 
