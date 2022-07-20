0219a9ee77c780d97c951c72494aa426b353c8a7
一骑孤烟 <996483674@qq.com>
2022-07-20 16:45:30 +0800

    Unity高版注入失效 (#995)
    * fix Unity高版本 Hotfix示例打包失效
    
    * fix Unity高版本打包 泛型注入失败

---
+++ Assets/XLua/Examples/08_Hotfix/Editor/HotfixTestCfg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using XLua;
+
+namespace XLuaTest.Editor
+{
+    public static class HotfixTestCfg
+    {
+        [Hotfix]
+        public static List<Type> by_field = new List<Type>()
+        {
+            typeof(HotfixTest),
+            typeof(HotfixCalc),
+            typeof(GenericClass<>),
+            typeof(InnerTypeTest),
+            typeof(BaseTest),
+            typeof(StructTest),
+            typeof(GenericStruct<>),
+            typeof(StatefullTest)
+        };
+
+    }
+}
