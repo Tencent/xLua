7b985bfc545eb257d17ac3e664a0fb1c7924d613
云却 <479813005@qq.com>
2024-04-21 22:19:06 +0800

    增加异步函数热更示例

--- Assets/XLua/Examples/14_HotfixAsyncAwait/HotfixAsyncAwaitTest.cs
+++ Assets/XLua/Examples/14_HotfixAsyncAwait/HotfixAsyncAwaitTest.cs
@@ -151,6 +151,141 @@ namespace XLuaTest
             // 执行脚本
             luaEnv.DoString(MyTask2Lua.text);
         }
+
+
+        void OnGUI()
+        {
+            Vector2 size = new Vector2(200, 40);
+            Vector2 position = new Vector2(600, 100);
+            Vector2 rightPposition = position + new Vector2(240, 0);
+            int inteval = 50;
+
+
+            if (GUI.Button(new Rect(position, size), nameof(Method1)))
+            {
+                Method1();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixMethod1)))
+            {
+                DoHotfixMethod1();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(AsyncMethod1)))
+            {
+                AsyncMethod1();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixAsyncMethod1)))
+            {
+                DoHotfixAsyncMethod1();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(AsyncMethod2)))
+            {
+                AsyncMethod2();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixAsyncMethod2)))
+            {
+                DoHotfixAsyncMethod2();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(AsyncMethod3)))
+            {
+                AsyncMethod3();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixAsyncMethod3)))
+            {
+                DoHotfixAsyncMethod3();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(AsyncMethod4)))
+            {
+                AsyncMethod4();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixAsyncMethod4)))
+            {
+                DoHotfixAsyncMethod4();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(MyTask)))
+            {
+                MyTask();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixMyTask)))
+            {
+                DoHotfixMyTask();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(MyTask1)))
+            {
+                MyTask1();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixMyTask1)))
+            {
+                DoHotfixMyTask1();
+            }
+
+            position.y += inteval;
+            rightPposition.y += inteval;
+
+            if (GUI.Button(new Rect(position, size), nameof(MyTask2)))
+            {
+                MyTask2();
+            }
+
+            if (GUI.Button(new Rect(rightPposition, size), nameof(DoHotfixMyTask2)))
+            {
+                DoHotfixMyTask2();
+            }
+
+
+            string chHint = @"在运行该示例之前，请细致阅读xLua文档，并执行以下步骤：
+
+1.宏定义：添加 HOTFIX_ENABLE 到 'Edit > Project Settings > Player > Other Settings > Scripting Define Symbols'。
+（注意：各平台需要分别设置）
+
+2.生成代码：执行 'XLua > Generate Code' 菜单，等待Unity编译完成。
+
+3.注入：执行 'XLua > Hotfix Inject In Editor' 菜单。注入成功会打印 'hotfix inject finish!' 或者 'had injected!' 。";
+            string enHint = @"Read documents carefully before you run this example, then follow the steps below:
+
+1. Define: Add 'HOTFIX_ENABLE' to 'Edit > Project Settings > Player > Other Settings > Scripting Define Symbols'.
+(Note: Each platform needs to set this respectively)
+
+2.Generate Code: Execute menu 'XLua > Generate Code', wait for Unity's compilation.
+
+
+3.Inject: Execute menu 'XLua > Hotfix Inject In Editor'.There should be 'hotfix inject finish!' or 'had injected!' print in the Console if the Injection is successful.";
+            GUIStyle style = GUI.skin.textArea;
+            style.normal.textColor = Color.red;
+            style.fontSize = 16;
+            GUI.TextArea(new Rect(10, 100, 500, 290), chHint, style);
+            GUI.TextArea(new Rect(10, 400, 500, 290), enHint, style);
+        }
     }
 }
 
